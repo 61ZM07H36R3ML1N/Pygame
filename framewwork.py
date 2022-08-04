1ca8dbd454b9eee21f49ef16033c9e522817ff86
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-08-03 22:10:14 -0700

    code chunk commit

--- framewwork.py
+++ framewwork.py
@@ -2,3 +2,9 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
+
+while True:
+    for event in pygame.event.get():
+    # draw all our elements
+    # update everything
+        pygame.display.update()
